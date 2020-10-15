5a725f484f01495ec523d622d95078c683b09507
MichaelNormand <michael.normand@hotmail.com>
2020-10-15 10:57:11 -0400

    Fixing variables integrations

--- Robus/src/PID.cpp
+++ Robus/src/PID.cpp
@@ -1,5 +1,30 @@
 #include <PID.h>
 bool cyclesFinished = false;
+unsigned long lastCycle = 0;
+unsigned long actualTime = 0;
+unsigned long cycleDelay = 10;
+float pids[2] = {0, 0};
+float pid = 0.0;
+float overflowLeft = 0.0;
+float overflowRight = 0.0;
+float encoderLeft = 0.0;
+float encoderRight = 0.0;
+int32_t encoderRead = 0;
+int32_t cycle = 1;
+int32_t goal = 400;
+int32_t actualSpeed = 10;
+int32_t error = 0;
+int32_t lastErrors[2] = {goal, goal};
+int32_t lastError = 0;
+int32_t nextError = 0;
+int leftDirection = 1;
+int rightDirection = 1;
+float rightMotorTravel = 0.0;
+float leftMotorTravel = 0.0;
+float rightTraveledDistance = 0.0;
+float leftTraveledDistance = 0.0;
+int speedIncrease = 1;
+float turningPoint = 0.0;
 
 void PID(int motorToCheck, float &encoder, int direction)
 {
