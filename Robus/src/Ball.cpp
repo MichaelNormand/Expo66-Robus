9417596a821b32ae4bf292d56130eeee52968f90
Louis1David <72098213+Louis1David@users.noreply.github.com>
2020-11-01 13:08:26 -0500

    Update Ball.cpp
    Manque capteur couleur + pince

--- Robus/src/Ball.cpp
+++ Robus/src/Ball.cpp
@@ -1,13 +1,55 @@
 #include "Ball.h"
 #include "Distance.h"
 
-void BALL_Init(void){
-    //delay(15000) //15 second delay
-    AddLength(100);
+void BALL_Init()
+{
+    BoardInit();
+    Serial.begin(9600);
+
+    int couleur;
+
+
+    //depart signal ?
+    AddLength(5);
+    delay(1000);
+    AddLength(30);
     AddRotation(90, ROTATE_RIGHT);
-    AddLength(50);
+    AddLength(25);
     AddRotation(90, ROTATE_LEFT);
+    AddLength(45);
+    // regarder couleur + stocker
+/*
+        if(jaune){
+            couleur = 0;
+        }else if(bleu){
+            couleur = 1;
+        }else if(rose){
+            couleur = 2;
+        }
+*/
+
+    couleur = 1; // test
+
+    AddLength(140);
+    // fermer pince
+    // lever balle
     
-    //ReadColor
-    Serial.println("Added Sequence !");
+        if(couleur == 0){ //jaune
+            AddLength(90);
+            AddRotation(90, ROTATE_RIGHT);
+            AddLength(25);
+        }
+        else if(couleur == 1){ // bleu
+            AddLength(190);  
+            AddRotation(90, ROTATE_LEFT);
+            AddLength(25);
+        }
+        else if(couleur == 2){ // rose
+            AddLength(270);
+            AddRotation(90, ROTATE_RIGHT);
+            AddLength(25);
+        }
+        
+    // descendre balle
+    // ouvrir pince
 }
